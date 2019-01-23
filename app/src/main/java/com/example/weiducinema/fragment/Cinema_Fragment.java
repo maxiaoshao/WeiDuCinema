538307b6bd2a9d0048e05e1889b92e230840d797
maxiaoshao <sxx0209>
2019-01-23 14:22:48 +0800

    底部按钮

---
+++ app/src/main/java/com/example/weiducinema/fragment/Cinema_Fragment.java
@@ -0,0 +1,26 @@
+package com.example.weiducinema.fragment;
+
+import com.example.weiducinema.R;
+import com.example.weiducinema.core.base.BaseFragment;
+
+/**
+ * Created by 邵新轩(轩少)
+ * function:
+ * on 2019/1/23
+ */
+public class Cinema_Fragment extends BaseFragment {
+    @Override
+    public String getPageName() {
+        return null;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.frag_cinema_layout;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
