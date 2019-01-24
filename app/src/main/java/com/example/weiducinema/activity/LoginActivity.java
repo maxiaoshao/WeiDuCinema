891f790464798b18d1f6992597b34a9767ba2803
maxiaoshao <sxx0209>
2019-01-24 08:53:46 +0800

    我的页面

---
+++ app/src/main/java/com/example/weiducinema/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.weiducinema.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bw.movie.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
