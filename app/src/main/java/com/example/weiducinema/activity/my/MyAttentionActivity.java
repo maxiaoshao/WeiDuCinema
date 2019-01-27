77f9e8ca50e54f27d1303d3fa2ec51ccf2cd6f18
Mingzhishi <16601053447@163.com>
2019-01-27 17:07:18 +0800

    添加个人关注

--- app/src/main/java/com/example/weiducinema/activity/my/MyAttentionActivity.java
+++ app/src/main/java/com/example/weiducinema/activity/my/MyAttentionActivity.java
@@ -1,15 +1,154 @@
 package com.example.weiducinema.activity.my;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.bw.movie.R;
+import com.example.weiducinema.activity.WDLoginActivity;
+import com.example.weiducinema.adapter.UserAttenAdaptet;
+import com.example.weiducinema.adapter.UserAttenChangAdaptet;
+import com.example.weiducinema.app.SpacesItemDecoration;
+import com.example.weiducinema.base.DataCall;
+import com.example.weiducinema.base.WDBaseActivity;
+import com.example.weiducinema.bean.Result;
+import com.example.weiducinema.bean.UserAttenBean;
+import com.example.weiducinema.bean.UserAttentionCheng;
+import com.example.weiducinema.bean.encrypt.UserInfo;
+import com.example.weiducinema.core.exception.ApiException;
+import com.example.weiducinema.db.DBManager;
+import com.example.weiducinema.precener.UserAttenChengPrencenter;
+import com.example.weiducinema.precener.UserAttenPrencenter;
 
-public class MyAttentionActivity extends AppCompatActivity {
+import java.sql.SQLException;
+import java.util.List;
+
+public class MyAttentionActivity extends WDBaseActivity {
+    RecyclerView recy;
+    Button pai,yuan;
+    private DBManager manager;
+    UserAttenChengPrencenter userAttenChengPrencenter;
+    private List<UserInfo> student;
+    UserAttenPrencenter userAttenPrencenter;
+    UserAttenAdaptet attenAdaptet;
+    UserAttenChangAdaptet attenAdaptets;
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_my__attention_;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my__attention_);
+    protected void initView() {
+        recy = findViewById(R.id.recy);
+        pai = findViewById(R.id.bt_pian);
+        yuan = findViewById(R.id.bt_cheng);
+
+        recy = findViewById(R.id.ercy);
+        userAttenPrencenter = new UserAttenPrencenter(new UserAtten());
+        userAttenChengPrencenter = new UserAttenChengPrencenter(new UserAttenChang());
+
+        try {
+
+            manager = new DBManager(MyAttentionActivity.this);
+
+            student = manager.getStudent();
+            if (student.size()==0){
+                startActivity(new Intent(MyAttentionActivity.this,WDLoginActivity.class));
+            }else{
+                pai.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        userAttenPrencenter.reqeust(student.get(0).getUserId()+"",student.get(0).getSessionId()+"","1","10");
+                    }
+                });
+                yuan.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        userAttenChengPrencenter.reqeust(student.get(0).getUserId()+"",student.get(0).getSessionId()+"","1","10");
+
+                    }
+                });
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+            userAttenPrencenter.reqeust(student.get(0).getUserId()+"",student.get(0).getSessionId()+"","1","10");
+            attenAdaptet = new UserAttenAdaptet(this);
+            attenAdaptets = new UserAttenChangAdaptet(this);
+            recy.addItemDecoration(new SpacesItemDecoration(20));
+            recy.setLayoutManager(new LinearLayoutManager(this));
+            recy.setAdapter(attenAdaptet);
+
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private class UserAtten implements DataCall<Result<List<UserAttenBean>>> {
+        @Override
+        public void success(Result<List<UserAttenBean>> data) {
+            if (data.getStatus().equals("0000")){
+                attenAdaptet.setData(data.getResult());
+                attenAdaptet.notifyDataSetChanged();
+            }else{
+                for(int i = 0;i<student.size();i++){
+                    try {
+                        manager.deleteStudent(student.get(i));
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(MyAttentionActivity.this,WDLoginActivity.class));
+
+            }
+        }
+
+        @Override
+        public void fail(ApiException e) {
+
+        }
+    }
+
+    private class UserAttenChang implements DataCall<Result<List<UserAttentionCheng>>> {
+
+
+        @Override
+        public void success(Result<List<UserAttentionCheng>> data) {
+            if (data.getStatus().equals("0000")){
+                attenAdaptets.setData(data.getResult());
+                attenAdaptets.notifyDataSetChanged();
+            }else{
+                for(int i = 0;i<student.size();i++){
+                    try {
+                        manager.deleteStudent(student.get(i));
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(MyAttentionActivity.this,WDLoginActivity.class));
+
+            }
+        }
+
+        @Override
+        public void fail(ApiException e) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/weiducinema/adapter/UserAttenChangAdaptet.java
@@ -0,0 +1,63 @@
+package com.example.weiducinema.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bw.movie.R;
+import com.example.weiducinema.activity.my.MyAttentionActivity;
+import com.example.weiducinema.bean.UserAttenBean;
+import com.example.weiducinema.bean.UserAttentionCheng;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * created by fxb
+ * 2019/1/27 16:50
+ */
+public class UserAttenChangAdaptet extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private List<UserAttentionCheng> li;
+    public UserAttenChangAdaptet(MyAttentionActivity myAttentionActivity) {
+        this.context = myAttentionActivity;
+        li = new ArrayList<>();
+    }
+    public void setData(List<UserAttentionCheng> result) {
+        li = result;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = View.inflate(context,R.layout.userattent_adapter,null);
+        return new ViewHelow1(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        ViewHelow1 viewHelow1 = (ViewHelow1) viewHolder;
+        viewHelow1.sim.setImageURI(li.get(i).getLogo());
+        viewHelow1.name.setText(li.get(i).getName());
+        viewHelow1.jie.setText(li.get(i).getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return li.size();
+    }
+    class ViewHelow1 extends RecyclerView.ViewHolder {
+        SimpleDraweeView sim;
+        TextView name,jie;
+        public ViewHelow1(@NonNull View itemView) {
+            super(itemView);
+            sim = itemView.findViewById(R.id.sim);
+            name = itemView.findViewById(R.id.name);
+            jie = itemView.findViewById(R.id.jie);
+        }
+    }
+
+}
