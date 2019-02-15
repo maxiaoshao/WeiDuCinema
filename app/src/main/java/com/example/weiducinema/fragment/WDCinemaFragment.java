194012b43a8646399aa53f107763431795cd03fe
Mingzhishi <16601053447@163.com>
2019-02-15 20:22:29 +0800

    添加布局，定位

--- app/src/main/java/com/example/weiducinema/fragment/WDCinemaFragment.java
+++ app/src/main/java/com/example/weiducinema/fragment/WDCinemaFragment.java
@@ -1,13 +1,23 @@
 package com.example.weiducinema.fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.bw.movie.R;
 import com.example.weiducinema.activity.WDLoginActivity;
 import com.example.weiducinema.activity.WDShowmoiver;
@@ -45,9 +55,12 @@ public class WDCinemaFragment extends WDBaseFragment implements YuanTuiAdaptr.on
     Dao<UserInfo, String> userDao;
     List<UserInfo> student;
     YuanTuiAdaptr adaptr;
+    TextView text11;
     GuanCinmearPersent guanCinmearPersent;
     YuanCinemaFuPersent yuanCinemaFuPersent;
     YuanCinemaPersent yuanCinemaPersent;
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
     @Override
     public String getPageName() {
         return null;
@@ -66,7 +79,7 @@ public class WDCinemaFragment extends WDBaseFragment implements YuanTuiAdaptr.on
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+text11 = view.findViewById(R.id.text111);
         tui = view.findViewById(R.id.tui);
         fu = view.findViewById(R.id.fu);
         adaptr = new YuanTuiAdaptr(getActivity());
@@ -109,6 +122,36 @@ public class WDCinemaFragment extends WDBaseFragment implements YuanTuiAdaptr.on
 
             }
         });
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.READ_PHONE_STATE
+                }, 100);
+            } else {
+                mLocationClient = new LocationClient(getContext());
+                //声明LocationClient类
+                mLocationClient.registerLocationListener(myListener);
+                //注册监听函数
+                LocationClientOption option = new LocationClientOption();
+                option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
+                //可选，是否需要位置描述信息，默认为不需要，即参数为false
+                //如果开发者需要获得当前点的位置信息，此处必须为true
+                option.setIsNeedLocationDescribe(true);
+                //可选，设置是否需要地址信息，默认不需要
+                option.setIsNeedAddress(true);
+                //可选，默认false,设置是否使用gps
+                option.setOpenGps(true);
+                //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+                option.setLocationNotify(true);
+                mLocationClient.setLocOption(option);
+                mLocationClient.start();
+            }
+        }
     }
 
     @Override
@@ -186,5 +229,20 @@ public class WDCinemaFragment extends WDBaseFragment implements YuanTuiAdaptr.on
         public void fail(ApiException e) {
 
         }
+    }
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+            //以下只列举部分获取地址相关的结果信息
+            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
+            String addr = location.getCity();    //获取详细地址信息
+            if (addr != null && addr != "") {
+                text11.setText(addr);
+            }
+
+        }
+
     }
 }
